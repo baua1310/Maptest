09ec4cc5f3631ea17bb77b4dee65c0e921396ae8
Julian Bauer <bauer.jb@gmx.de>
2016-06-19 19:51:15 +0200

    an Schärding angepasst

--- app/src/main/java/com/example/bauer/maptest/MapsActivity.java
+++ app/src/main/java/com/example/bauer/maptest/MapsActivity.java
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -24,28 +25,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
+
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
         // Add a marker in Sydney, Australia, and move the camera.
-        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        LatLng sydney = new LatLng(48.455939, 13.434578);
-        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        LatLng sydney2 = new LatLng(48.455533, 13.428804);
-        map.addMarker(new MarkerOptions().position(sydney2).title("Marker in Sydney 2"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney2));
+        map.setMapType(GoogleMap.MAP_TYPE_NONE);
+        LatLngBounds SCHAERDING = new LatLngBounds(
+                new LatLng(48.452018, 13.423420), new LatLng(48.460707, 13.436554));  //southwest, northeast
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(SCHAERDING.getCenter(), 17));
+        LatLng schaerding = new LatLng(48.455939, 13.434578);
+        map.addMarker(new MarkerOptions().position(schaerding).title("Schärding"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(schaerding));
 
         GroundOverlayOptions newarkMap = new GroundOverlayOptions()
-                .image(BitmapDescriptorFactory.fromResource(R.drawable.maptest))
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.schaerding))
                 .anchor(0, 0)
-                .position(new LatLng(48.461970, 13.425449), 1370f, 1370f);
+                .position(new LatLng(48.460707, 13.423420), 962f, 962f);
 
         GroundOverlay imageOverlay = map.addGroundOverlay(newarkMap);
     }
-
-
 }
 
